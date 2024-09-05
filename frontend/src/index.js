01b3019323c09672c8a415959ff876eb38f334f6
Eduardo fernandes da silva <eduardo.fernandes.silva13@gmail.com>
2024-09-05 17:02:46 -0300

    Criação da tela inicial e da implementação do tailwind e postcss

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.js'; 
+import App from './App.js';
+import './index.css';
 
 const container = document.getElementById('root');
 if (container) {
-	const root = createRoot(container); 
-	root.render(<App/>);
+	const root = createRoot(container);
+	root.render(<App />);
 } else {
 	console.error('No element with ID "app" found in the DOM.');
 }
