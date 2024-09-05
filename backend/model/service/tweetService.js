bfff25eb626cb922f2caac4499308eea4f65b20d
wendrellBr <wendrellpossatooliveira@gmail.com>
2024-09-05 16:33:30 -0300

    :bug: fix: Adicionado logs de erro no TweetService

--- backend/model/service/tweetService.js
+++ backend/model/service/tweetService.js
@@ -5,15 +5,17 @@ const tweetRepository = new TweetRepository();
 export class TweetService {
 
   async createTweet(tweet) {
+    console.log("\n\n\ninfo: Iniciado TweetService.createTweet", tweet); // Debug
+
     try {
 
       //Validação de dados temporárias 
-      
-            // // Verifique se o autor do tweet existe (inativo ate os user estarem cadastrados)
-            // const authorExists = await userRepository.findUserById(tweet.authorId);
-            // if (!authorExists) {
-            //   throw new Error('Author does not exist');
-            // }
+
+      // // Verifique se o autor do tweet existe (inativo ate os user estarem cadastrados)
+      // const authorExists = await userRepository.findUserById(tweet.authorId);
+      // if (!authorExists) {
+      //   throw new Error('Author does not exist');
+      // }
 
       // Verifique se o texto do tweet não está vazio
       if (!tweet.text || tweet.text.trim() === '') {
@@ -21,41 +23,47 @@ export class TweetService {
       }
 
       // Verifica a data de criação do tweet
-      if (!tweet.createdAt || isNaN(new Date(tweet.createdAt).getTime())) { 
+      if (!tweet.createdAt || isNaN(new Date(tweet.createdAt).getTime())) {
         tweet.createdAt = new Date(); // Se veradeiro, use a data atual
       }
 
-
+      console.log("\n\n\ninfo: Finalizado TweetService.createTweet", tweet); // Debug
       return await tweetRepository.createTweet(tweet);
     } catch (error) {
-      console.error('Error creating tweet:', error);
+      console.log("\n\n\nerror: TweetService.createTweet", error); // Debug
       throw new Error('Failed to create tweet');
     }
   }
 
   async findTweetById(id) {
+    console.log("\n\n\ninfo: Iniciado TweetService.findTweetById", id); // Debug
     try {
+      console.log("\n\n\ninfo: Finalizado TweetService.findTweetById", id); // Debug
       return await tweetRepository.findTweetById(id);
     } catch (error) {
-      console.error('Error finding tweet by ID:', error);
+      console.log("\n\n\nerror: TweetService.findTweetById", error); // Debug
       throw new Error('Failed to find tweet');
     }
   }
 
   async findAllTweets() {
+    console.log("\n\n\ninfo: Iniciado TweetService.findAllTweets"); // Debug
     try {
+      console.log("\n\n\ninfo: Finalizado TweetService.findAllTweets"); // Debug
       return await tweetRepository.findAllTweets();
     } catch (error) {
-      console.error('Error finding all tweets:', error);
+      console.log("\n\n\nerror: TweetService.findAllTweets", error); // Debug
       throw new Error('Failed to find tweets');
     }
   }
 
   async findTweetsByAuthorId(authorId) {
+    console.log("\n\n\ninfo: Iniciado TweetService.findTweetsByAuthorId", authorId); // Debug
     try {
+      console.log("\n\n\ninfo: Finalizado TweetService.findTweetsByAuthorId", authorId); // Debug
       return await tweetRepository.findTweetsByAuthorId(authorId);
     } catch (error) {
-      console.error('Error finding tweets by author ID:', error);
+      console.log("\n\n\nerror: TweetService.findTweetsByAuthorId", error); // Debug
       throw new Error('Failed to find tweets by author');
     }
   }
