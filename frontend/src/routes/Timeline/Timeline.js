821cd83a4ba68ed8b90c6f01ca3046f3484523c0
Pedro Rodrigues <pedrorodriguesssrrr@gmail.com>
2024-09-10 17:57:32 -0300

    import navbar ajustado

--- frontend/src/routes/Timeline/Timeline.js
+++ frontend/src/routes/Timeline/Timeline.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Navbar from '../../components/Navbar.js';
+import Navbar from '../../components/navbar.js';
 import Explorebar from '../../components/Explorebar.js';
 
 export default function Timeline() {
