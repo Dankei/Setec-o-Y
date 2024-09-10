9ed463e195ea85f4cac46ae25d3191b2b6765994
Eduardo fernandes da silva <eduardo.fernandes.silva13@gmail.com>
2024-09-10 13:30:50 -0300

    Completei a questão de responsividade da tela inicial
    Deixei as telas: inicial, cadastro, login, responsivas e com animações, mais agradáveis ao usuário.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,9 @@ import React from "react"
 
 function App() {
     return (
-        <div className="w-screen h-screen">
+        <>
             <Outlet />
-        </div>
+        </>
     )
 }
 
--- frontend/src/pages/SignUpScreen.js
+++ frontend/src/pages/SignUpScreen.js
@@ -1,79 +1,125 @@
-import React from 'react';
-import { useState } from 'react';
-
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
 
 function SignUpScreen({ onLoginClick }) {
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [repeatPassword, setRepeatPassword] = useState('');
+    const [focusedInput, setFocusedInput] = useState('');
 
-    const handleUsernameChange = (event) => {
-        setUsername(event.target.value);
-    }
+    const handleFocus = (field) => {
+        setFocusedInput(field);
+    };
 
-    const handleEmailChange = (event) => {
-        setEmail(event.target.value);
-    }
-
-    const handlepasswordChange = (event) => {
-        setPassword(event.target.value);
-    }
+    const handleBlur = () => {
+        setFocusedInput('');
+    };
 
     return (
-        <div className="flex absolute justify-center flex-col items-center text-white top-0 bottom-0 left-0 right-0 gap-10">
-            <h1 className="2xl:text-7xl lg:text-5xl  text-4xl font-black">Criar conta</h1>
-            <form action="" className="flex flex-col gap-10 w-1/2">
-                <div className="flex flex-col ">
-                    <input type="text" id="nome" placeholder="@fulanodetal" onChange={handleUsernameChange}  className="border-b-[3px] bg-transparent w-full px-5 py-1 text-2xl focus:outline-none" />
-                    <label htmlFor="nome" className="px-5 text-2xl font-thin">Nome de Usuário</label>
+        <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -50 }}
+            transition={{ duration: 0.5 }}
+            className="absolute top-0 bottom-0 left-0 right-0 flex flex-col items-center justify-center max-h-full gap-10 text-white"
+        >
+            <h1 className="text-4xl font-black 2xl:text-7xl lg:text-5xl">Criar conta</h1>
+            <form action="" className="flex flex-col gap-10">
+                <div className="flex flex-col flex-1 w-full gap-10 md:flex-row">
+                    <div className="relative flex flex-col">
+                        <input
+                            type="text"
+                            id="nome"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            onFocus={() => handleFocus('username')}
+                            onBlur={handleBlur}
+                            className="border-b-[3px] bg-transparent w-full px-5 py-1 text-2xl focus:outline-none transition duration-300 ease-in-out transform focus:scale-105 focus:border-white"
+                        />
+                        <label
+                            htmlFor="nome"
+                            className={`absolute px-5 text-2xl font-thin transition-all duration-300 ease-in-out transform ${focusedInput === 'username' || username
+                                ? 'translate-y-[-100%] opacity-0'
+                                : 'translate-y-0 opacity-100'
+                                }`}
+                        >
+                            Nome de Usuário
+                        </label>
+                    </div>
+                    <div className="relative flex flex-col">
+                        <input
+                            type="email"
+                            id="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            onFocus={() => handleFocus('email')}
+                            onBlur={handleBlur}
+                            className="border-b-[3px] bg-transparent w-full px-5 py-1 text-2xl focus:outline-none transition duration-300 ease-in-out transform focus:scale-105 focus:border-white"
+                        />
+                        <label
+                            htmlFor="email"
+                            className={`absolute px-5 text-2xl font-thin transition-all duration-300 ease-in-out transform ${focusedInput === 'email' || email
+                                ? 'translate-y-[-100%] opacity-0'
+                                : 'translate-y-0 opacity-100'
+                                }`}
+                        >
+                            E-mail
+                        </label>
+                    </div>
                 </div>
-                <div className="flex flex-col w-full">
-                    <input type="text" id="nome" onChange={handleEmailChange} placeholder="Digite aqui o seu melhor e-mail" className="border-b-[3px] bg-transparent w-full px-5 py-1 text-2xl focus:outline-none" />
-                    <label htmlFor="nome" className="px-5 text-2xl font-thin">E-mail</label>
-                </div>
-                <div className="flex flex-col w-full">
-                    <input type="password" id="nome" onChange={handlepasswordChange}  placeholder="****************" className="border-b-[3px] bg-transparent w-full px-5 py-1 text-2xl focus:outline-none" />
-                    <label htmlFor="nome" className="px-5 text-2xl font-thin">password</label>
+                <div className="flex flex-col w-full gap-10 md:flex-row">
+                    <div className="relative flex flex-col">
+                        <input
+                            type="password"
+                            id="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            onFocus={() => handleFocus('password')}
+                            onBlur={handleBlur}
+                            className="border-b-[3px] bg-transparent w-full px-5 py-1 text-2xl focus:outline-none transition duration-300 ease-in-out transform focus:scale-105 focus:border-white"
+                        />
+                        <label
+                            htmlFor="password"
+                            className={`absolute px-5 text-2xl font-thin transition-all duration-300 ease-in-out transform ${focusedInput === 'password' || password
+                                ? 'translate-y-[-100%] opacity-0'
+                                : 'translate-y-0 opacity-100'
+                                }`}
+                        >
+                            Senha
+                        </label>
+                    </div>
+                    <div className="relative flex flex-col">
+                        <input
+                            type="password"
+                            id="repeatPassword"
+                            value={repeatPassword}
+                            onChange={(e) => setRepeatPassword(e.target.value)}
+                            onFocus={() => handleFocus('repeatPassword')}
+                            onBlur={handleBlur}
+                            className="border-b-[3px] bg-transparent w-full px-5 py-1 text-2xl focus:outline-none transition duration-300 ease-in-out transform focus:scale-105 focus:border-white"
+                        />
+                        <label
+                            htmlFor="repeatPassword"
+                            className={`absolute px-5 text-2xl font-thin transition-all duration-300 ease-in-out transform ${focusedInput === 'repeatPassword' || repeatPassword
+                                ? 'translate-y-[-100%] opacity-0'
+                                : 'translate-y-0 opacity-100'
+                                }`}
+                        >
+                            Confirme a Senha
+                        </label>
+                    </div>
                 </div>
             </form>
-            <div className="w-full flex-col flex gap-10 my-5">
+            <div className="flex flex-col w-full gap-10 my-5">
                 <button
                     className="border-[3px] w-fit mx-auto px-14 py-5 rounded-full text-2xl transition-all hover:bg-white hover:text-black font-bold"
                     type="submit"
-                    onClick={() => {
-                        fetch('http://localhost:3001/api/users', {
-                            method: 'POST',
-                            headers: {
-                                'Content-Type': 'application/json',
-                            },
-                            body: JSON.stringify({
-                                username: username,
-                                email: email,
-                                password: password,
-                            }),
-                        })
-                            .then((response) => {
-                                if (!response.ok) {
-                                    // Exemplo de erro baseado no status HTTP
-                                    return response.text().then(text => {
-                                        throw new Error(text || 'Network response was not ok');
-                                    });
-                                }
-                                return response.json(); // ou response.text() dependendo do tipo de resposta esperado
-                            })
-                            .then((data) => {
-                                console.log('Response data:', data);
-                            })
-                            .catch((error) => {
-                                // O 'error' é o objeto de erro capturado no 'catch'
-                                console.error('Fetch error:', error.message);
-                            });
-                    }}
                 >
                     Criar conta
                 </button>
                 <hr className="sm:mx-20 mx-14 lg:mx-28" />
-                <h2 className="2xl:text-3xl text-2xl font-light text-center">Já possui uma conta?</h2>
+                <h2 className="text-2xl font-light text-center 2xl:text-3xl">Já possui uma conta?</h2>
                 <button
                     className="border-[3px] w-fit mx-auto px-14 py-5 rounded-full text-2xl transition-all hover:bg-white hover:text-black font-bold"
                     onClick={onLoginClick}
@@ -81,8 +127,8 @@ function SignUpScreen({ onLoginClick }) {
                     Fazer Login
                 </button>
             </div>
-        </div>
-    )
+        </motion.div>
+    );
 }
 
 export default SignUpScreen;
