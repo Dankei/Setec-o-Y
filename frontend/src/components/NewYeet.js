849da7d4c9f3cbff255ed389a7278945182ba1a1
Eduardo <edudfrs@gmail.com>
2024-09-12 20:21:49 -0300

    Alterei os icones pela inicial do criador do tweet

--- frontend/src/components/NewYeet.js
+++ frontend/src/components/NewYeet.js
@@ -18,71 +18,77 @@ export default function NewYeet() {
             setCharCount(newText.length); // Atualiza o comprimento do texto
         }
     }
-    return(
+
+    const userInitial = JSON.parse(localStorage.getItem('user'));
+    return (
         <>
             <div className="flex flex-col border-y-[1px] border-gray-600 p-4">
                 <div className="flex">
-                    <img className="size-14 rounded-full" src="/assets/images/user-icon.jpg"/>
+                    <div className="rounded-full w-fit bg-slate-200 h-fit">
+                        <p className="pt-1 text-lg font-bold text-center size-10 text-slate-500">
+                            {userInitial.username[0]}
+                        </p>
+                    </div>
                     <textarea
                         value={text}
                         onChange={handleTextChange}
                         type="text"
                         placeholder="O quê está acontecendo?"
-                        className="flex-1 h-16 mx-5 text-lg bg-slate-950 text-white  focus:outline-none focus:ring-2 focus:ring-slate-950"
+                        className="flex-1 h-16 mx-5 text-lg text-white bg-slate-950 focus:outline-none focus:ring-2 focus:ring-slate-950"
                     />
                 </div>
-                <div className=" flex justify-end items-center space-x-3 mt-2">
+                <div className="flex items-center justify-end mt-2 space-x-3 ">
                     <p className="text-white">
                         {charCount}/140
                     </p>
-                    
-                    <ButtonCommon 
-                    onClick={() => 
-                        {
+
+                    <ButtonCommon
+                        onClick={() => {
                             console.log(length.text);
-                            if(charCount == 0){
+                            if (charCount == 0) {
                                 setOpen(true)
-                            } else{
-                        axios.post('http://localhost:3001/api/tweets', {
-                            text: text,
-                            authorID: user.id
-                        }, {
-                            headers: {
-                                'Content-Type': 'application/json',
-                            }
-                        })
-                        .then((response) => {
-                            console.log('Response data:', response.data);
-                            window.location.reload();
-                        })
-                        .catch((error) => {
-                            if (error.response) {
-                                // O servidor respondeu com um status diferente de 2xx
-                                console.error('Error response:', error.response.data);
-                            } else if (error.request) {
-                                // A requisição foi feita, mas nenhuma resposta foi recebida
-                                console.error('Error request:', error.request);
                             } else {
-                                // Algo aconteceu ao configurar a requisição
-                                console.error('Error message:', error.message);
+                                axios.post('http://localhost:3001/api/tweets', {
+                                    text: text,
+                                    authorID: user.id
+                                }, {
+                                    headers: {
+                                        'Content-Type': 'application/json',
+                                    }
+                                })
+                                    .then((response) => {
+                                        console.log('Response data:', response.data);
+                                        window.location.reload();
+                                    })
+                                    .catch((error) => {
+                                        if (error.response) {
+                                            // O servidor respondeu com um status diferente de 2xx
+                                            console.error('Error response:', error.response.data);
+                                        } else if (error.request) {
+                                            // A requisição foi feita, mas nenhuma resposta foi recebida
+                                            console.error('Error request:', error.request);
+                                        } else {
+                                            // Algo aconteceu ao configurar a requisição
+                                            console.error('Error message:', error.message);
+                                        }
+                                    });
                             }
-                        });
-                    }}}
-                    type="submit" 
-                    text="Post" />
+                        }}
+                        type="submit"
+                        text="Post" />
                 </div>
-                
+
             </div>
-            
-            <PopUp text="Digite algo para postar!" open={open} onClose={()=> setOpen(false)}>
-                <div className="space-y-2 flex flex-col items-center">
+
+            <PopUp text="Digite algo para postar!" open={open} onClose={() => setOpen(false)}>
+                <div className="flex flex-col items-center space-y-2">
                     <p className="text-lg font-bold">
                         Você deve inserir algo para yeetar!
                     </p>
-                        <ButtonCommon onClick={() => setOpen(false)}text="Entendi"/>
+                    <ButtonCommon onClick={() => setOpen(false)} text="Entendi" />
                 </div>
-                </PopUp>
-            
+            </PopUp>
+
         </>
     );
 }
--- frontend/src/components/Yeet.js
+++ frontend/src/components/Yeet.js
@@ -4,58 +4,61 @@ import DeleteYeet from './DeleteYeet.js';
 
 export default function Yeet(props) {
     const [open, setOpen] = React.useState(false);
-    return(
+    const userInitial = JSON.parse(localStorage.getItem('user'));
+    return (
         <>
             <div className="flex flex-col border-y-[1px] border-gray-600 p-4">
-                <div className="flex  text-white">
-                    
-                    
-                    <div>
-                        <img className="size-12 rounded-full" src="/assets/images/user-icon.jpg"/>
+                <div className="flex text-white">
+
+
+                    <div className="rounded-full w-fit bg-slate-200 h-fit">
+                        <p className="pt-1 text-lg font-bold text-center size-10 text-slate-500">
+                            {userInitial.username[0]}
+                        </p>
                     </div>
-                    
+
                     <div className="ms-4 text-white w-[30rem]">
                         <div className="flex justify-between ">
                             <p className="text-sm"><b>{props.User}</b> - {props.Date} </p>
-                            <button 
-                            onClick={() => setOpen(true)}
+                            <button
+                                onClick={() => setOpen(true)}
                             ><img src="/assets/images/more.png"></img></button>
                         </div>
-                        
 
 
-                        <p className=" ">{props.Content}</p>
+
+                        <p className="">{props.Content}</p>
                         <div className="flex justify-between mt-5 w-[25rem]">
-                            
+
                             <div className="flex space-x-2">
-                                <p><img src="/assets/images/comment.png"/></p>
-                                <p>0</p>
-                            </div>
-                            
-                            <div className="flex space-x-2">
-                                <p><img src="/assets/images/rt-unselected.png"/></p>
+                                <p><img src="/assets/images/comment.png" /></p>
                                 <p>0</p>
                             </div>
 
                             <div className="flex space-x-2">
-                                <p><img src="/assets/images/heart-unselected.png"/></p>
+                                <p><img src="/assets/images/rt-unselected.png" /></p>
                                 <p>0</p>
                             </div>
-                        
-                        <PopUp open={open} onClose={()=> setOpen(false)}>
-                            <DeleteYeet id={props.id} onClose={() => setOpen(false)} />
-                        </PopUp>
+
+                            <div className="flex space-x-2">
+                                <p><img src="/assets/images/heart-unselected.png" /></p>
+                                <p>0</p>
+                            </div>
+
+                            <PopUp open={open} onClose={() => setOpen(false)}>
+                                <DeleteYeet id={props.id} onClose={() => setOpen(false)} />
+                            </PopUp>
 
                         </div>
                     </div>
 
                 </div>
-                
-                
-                
+
+
+
             </div>
-            
-        
+
+
         </>
     );
 }
