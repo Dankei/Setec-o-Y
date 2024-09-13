bd85d3c07b516afe57a559757f0bcee3c5b85d8f
Eduardo fernandes da silva <eduardo.fernandes.silva13@gmail.com>
2024-09-13 09:35:43 -0300

    Tela de login / cadastro oficialmente pronta

--- frontend/src/routes/Timeline/HomePage.js
+++ frontend/src/routes/Timeline/HomePage.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import NewYeet from "../../components/NewYeet.js";
 import Yeet from "../../components/Yeet.js";
 import axios from 'axios';
+import {motion} from 'framer-motion';
 
 function HomePage() {
     const [tweets, setTweets] = useState([]);
@@ -17,7 +18,11 @@ function HomePage() {
     console.log(tweets)
 
     return (
-        <div className="flex flex-col h-full w-full border-solid border-gray-600 border-x-[1px]">
+        <motion.div initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -50 }}
+            transition={{ duration: 0.5 }}
+            className="flex flex-col h-full w-full border-solid border-gray-600 border-x-[1px]">
             <div className="flex py-5 text-xl font-thin text-white justify-evenly">
                 <p className="text-white">Todos</p>
                 <p className="text-white">Seguindo</p>
@@ -33,7 +38,7 @@ function HomePage() {
                     />
                 ))}
             </div>
-        </div>
+        </motion.div>
     );
 }
 
